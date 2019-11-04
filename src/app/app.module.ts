2e0deb18da83508f791eac451249b70d70929965
Spencer Laird <salaird0324@gmail.com>
2019-11-03 19:21:38 -0600

    billing, captcha, profile, proxies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material';
+import { BillingComponent } from './billing/billing.component';
+import { ProxiesComponent } from './proxies/proxies.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CaptchaComponent } from './captcha/captcha.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    BillingComponent,
+    ProxiesComponent,
+    ProfileComponent,
+    CaptchaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/proxies/proxies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proxies',
+  templateUrl: './proxies.component.html',
+  styleUrls: ['./proxies.component.css']
+})
+export class ProxiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
